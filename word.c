9838afbdd43f7f9826a612b3dfb7c7bf42b5d6ad
Cooper Hanson <hancooj@dunwoody.edu>
2023-04-26 15:49:26 -0500

    moved the web request to its own function

--- word.c
+++ word.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <curl/curl.h> // sudo apt install libcurl4-openssl-dev
 // https://youtu.be/x2PaEz1URso
+// // curl.se/libcurl # man libcurl // sudo apt install libcurl4-doc
+#include <jansson.h> // sudo apt install libjansson-dev
+//https://github.com/akheron/jansson
 
-int main() {
+CURLcode httpGet(char* url) {
   CURL* curl;
-  CURLcode resp;
+  CURLcode res;
 
   curl_global_init(CURL_GLOBAL_ALL);
   curl = curl_easy_init();
-  if (!curl) { fprintf(stderr, "Failed to initialize curl\n"); return 1; }
+  if (!curl) { fprintf(stderr, "Failed to initialize curl\n"); return CURLE_FAILED_INIT; }
 
-  curl_easy_setopt(curl, CURLOPT_URL, "https://random-word-api.herokuapp.com/word?number=1");
-  resp = curl_easy_perform(curl);
-  if (resp != CURLE_OK) {
-    fprintf(stderr, "Failed CURL request: %s\n", curl_easy_strerror(resp));
-    curl_easy_cleanup(curl); // do I need to cleanup here?
-    return 1;
-  }
+  curl_easy_setopt(curl, CURLOPT_URL, url);
+  res = curl_easy_perform(curl);
+  if (res != CURLE_OK)
+    { fprintf(stderr, "Failed CURL request: %s\n", curl_easy_strerror(res)); }
 
-  printf("%u\n", resp);
   curl_easy_cleanup(curl);
   curl_global_cleanup(); // do I need both cleanups?
-  return 0;
+  return res;
+}
+
+int main() {
+  char* word_endpoint = "https://random-word-api.herokuapp.com/word?number=1";
+  printf("%u\n", httpGet(word_endpoint));
 }
