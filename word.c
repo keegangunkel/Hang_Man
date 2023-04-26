a44eb12f4189c4c2db5c1b1844ca9057a2220e10
Cooper Hanson <hancooj@dunwoody.edu>
2023-04-26 17:34:48 -0500

    added source

--- word.c
+++ word.c
@@ -81,7 +81,7 @@ int main() {
   struct ResponseData word_data = httpGet(word_endpoint);
 
   json_t* json_obj = parse_json(word_data.data);
-  const char *word = json_string_value(json_array_get(json_obj, 0));
+  const char *word = json_string_value(json_array_get(json_obj, 0)); /* copied from https://stackoverflow.com/questions/47226401/how-to-extract-keyless-values-from-json-array-using-jansson */
   free(word_data.data);
 
   printf("result: %s\n", word);
