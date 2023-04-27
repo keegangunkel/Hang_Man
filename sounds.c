d2c6dd123fc6b4e47f1efcda7f49ad490424793a
Gunkel <gunkeec@dunwoody.edu>
2023-04-27 16:36:05 -0500

    Added a background music file if we want to use it and created a hypothetical way we can implement the sounds when George codes the correct and wrong guesses

--- sounds.c
+++ sounds.c
@@ -16,14 +16,39 @@ void play_wrong_sound() {
     play_sound(filename);
 }
 
-int main() {
-    // Play a correct sound
-    play_correct_sound();
-    
-    // Play a wrong sound
-    play_wrong_sound();
-    
-    return 0;
+
+void play_background_music(const char* filename) {
+    PlaySound(filename, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 }
 
+int main() {
+    // Start playing the background music in a separate thread
+    const char* bg_music_filename = "bg_music.wav";
+    PlaySound(bg_music_filename, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+
+// Below is hypothetical code so the sounds can be implemented it will most likely come into play when george codes the correct
+// and wrong guesses
+
+/*
+    // Game loop
+    while (1) {
+        // Get user input and update the game state
+
+        // If the user makes a correct guess, play the correct sound
+        if (user_makes_correct_guess()) {
+            play_correct_sound();
+        }
+        // If the user makes a wrong guess, play the wrong sound
+        else if (user_makes_wrong_guess()) {
+            play_wrong_sound();
+        }
+
+        // Check if the game is over
+        if (game_is_over()) {
+            break;
+        }
+    }
+*/
+    return 0;
+}
 
