3e56e51c113dbb7061ff0132f8aa3cb69315b2ed
Cooper Hanson <hancooj@dunwoody.edu>
2023-04-27 15:17:00 -0500

    random word function

--- main.c
+++ main.c
@@ -4,12 +4,25 @@
 
 int main() {
   printf("Random Word Generation example\n");
+/*
+ * Gets a random word from the internet
+ * Result must be free'd
+*/
+char* getRandomWord() {
   char* word_endpoint = "https://random-word-api.herokuapp.com/word?number=1";
   struct ResponseData word_data = httpGet(word_endpoint);
-
   json_t* json_obj = parse_json(word_data.data);
-  const char *word = json_string_value(json_array_get(json_obj, 0)); // copied from https://stackoverflow.com/questions/47226401/how-to-extract-keyless-values-from-json-array-using-jansson
+
+  // copied from https://stackoverflow.com/questions/47226401/how-to-extract-keyless-values-from-json-array-using-jansson
+  const char *word = json_string_value(json_array_get(json_obj, 0)); 
+  char* result = malloc(strlen(word) + 1);
+  strcpy(result, word);
+
   free(word_data.data);
+  json_decref(json_obj);
+  return result;
+}
+
 
   printf("result: %s\n", word);
   // Now let's get the definition
@@ -24,4 +37,10 @@ int main() {
   free(dict_data.data);
   json_decref(json_obj); // frees the word variable
   json_decref(dict_obj);
+int main() {
+  char* word = getRandomWord();
+  printf("The random word was %s\n", word);
+  free(word);
+  return 0;
+  
 }
