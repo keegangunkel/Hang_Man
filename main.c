b461b5206aa5edd32978200c396688df1344264f
Cooper Hanson <hancooj@dunwoody.edu>
2023-04-27 17:10:59 -0500

    can convert a random string from the network into a Word struct

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ typedef struct {
   char* letters;
   char* synonym;
   char* antonym;
+  char* definition;
 } Word;
 
 /*
@@ -27,30 +28,70 @@ char* getRandomWord() {
   return result;
 }
 
-/* This is not yet implemented */
+/*
+ * Copies string from one pointer to another
+ * @param dest - the destination to copy to. To malloc this needs to be a pointer to a pointer.
+ * @param src - The source string to copy into the destination
+*/
+void charPtrCpy(char** dest, const char* src) {
+  if (src == NULL) { return; }
+  *dest = malloc(strlen(src) + 1);
+  if (*dest == NULL) { 
+    fprintf(stderr, "Failed to allocate memory\n");
+    return;
+  }
+  strcpy(*dest, src);
+  return;
+}
+
 Word wordFromLetters(char* letters) {
   Word result = { 0 };
-
-  char dict_endpoint[50];
+  char dict_endpoint[100];
   sprintf(dict_endpoint, "https://api.dictionaryapi.dev/api/v2/entries/en/%s", letters);
+
   struct ResponseData dict_data = httpGet(dict_endpoint);
   json_t* dict_obj = parse_json(dict_data.data);
-  printf("");
+  free(dict_data.data);
+  const char* err = json_string_value(json_object_get(dict_obj, "title"));
+  if (err != NULL) {
+    // return early if error present
+    fprintf(stderr, "DEBUG: %s not found in dict\n", letters);
+    json_decref(dict_obj);
+    return result;
+  }
 
+  json_t* word_json = json_array_get(
+    json_object_get(
+      json_array_get(
+        dict_obj, 0),
+      "meanings"),
+    0);
+
+  const char* syn_str = json_string_value(json_array_get(json_object_get(word_json, "synonyms"), 0));
+  const char* ant_str = json_string_value(json_array_get(json_object_get(word_json, "antonyms"), 0));
+  const char* def_str = json_string_value(json_object_get(json_array_get(json_object_get(word_json, "definitions"), 0), "definition"));
+
+  charPtrCpy(&result.letters,    letters);
+  charPtrCpy(&result.synonym,    syn_str);
+  charPtrCpy(&result.antonym,    ant_str);
+  charPtrCpy(&result.definition, def_str);
+
+  json_decref(dict_obj);
   return result;
 }
-  /*
-  json_t* definition = json_array_get(json_object_get(json_array_get(json_object_get(json_array_get(dict_obj, 0), "meanings"), 0), "definitions"), 0);
-  printf("The dict data says: %s\n", json_string_value(json_object_get(definition, "definition")));
 
-  free(dict_data.data);
-  json_decref(dict_obj);
-  */
+void printWord(Word w) {
+  printf("Word(\n.letters = %s,\n.synonym = %s,\n.antonym = %s,\n.definition = %s\n)\n", w.letters, w.synonym, w.antonym, w.definition);
+  return;
+}
 
 int main() {
   char* word = getRandomWord();
-  printf("The random word was %s\n", word);
+  Word w = wordFromLetters(word);
   free(word);
+
+  printWord(w);
+
   return 0;
   
 }
