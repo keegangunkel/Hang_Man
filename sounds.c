c6d6e102a5681d52df6f95a95187cd20dbaa2385
Gunkel <gunkeec@dunwoody.edu>
2023-04-27 17:55:55 -0500

    Reworked all the code so it will be easier to implement. Currently fixing a bug where after you input it stops the background music.

--- sounds.c
+++ sounds.c
@@ -1,4 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <Windows.h>
+#include <conio.h>
+#include <process.h>
 #pragma comment(lib, "winmm.lib")
 
 
@@ -6,49 +10,152 @@ void play_sound(const char* filename) {
     PlaySound(filename, NULL, SND_FILENAME);
 }
 
+void play_sound_from_memory(void* sound_data, DWORD sound_size, DWORD flags) {
+    PlaySound(sound_data, NULL, SND_MEMORY | SND_ASYNC | flags);
+}
+
 void play_correct_sound() {
     const char* filename = "correct_sound.wav";
-    play_sound(filename);
+    HANDLE hFile = CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf("Failed to open file: %s\n", filename);
+        return;
+    }
+
+    DWORD sound_size = GetFileSize(hFile, NULL);
+    if (sound_size == INVALID_FILE_SIZE) {
+        printf("Failed to get file size: %s\n", filename);
+        CloseHandle(hFile);
+        return;
+    }
+
+    void* sound_data = malloc(sound_size);
+    if (sound_data == NULL) {
+        printf("Failed to allocate memory for sound data.\n");
+        CloseHandle(hFile);
+        return;
+    }
+
+    DWORD bytes_read;
+    if (!ReadFile(hFile, sound_data, sound_size, &bytes_read, NULL)) {
+        printf("Failed to read file: %s\n", filename);
+        free(sound_data);
+        CloseHandle(hFile);
+        return;
+    }
+
+    CloseHandle(hFile);
+
+    play_sound_from_memory(sound_data, sound_size, SND_ASYNC);
+
+    free(sound_data);
 }
 
 void play_wrong_sound() {
     const char* filename = "wrong_sound.wav";
-    play_sound(filename);
+    HANDLE hFile = CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf("Failed to open file: %s\n", filename);
+        return;
+    }
+
+    DWORD sound_size = GetFileSize(hFile, NULL);
+    if (sound_size == INVALID_FILE_SIZE) {
+        printf("Failed to get file size: %s\n", filename);
+        CloseHandle(hFile);
+        return;
+    }
+
+    void* sound_data = malloc(sound_size);
+    if (sound_data == NULL) {
+        printf("Failed to allocate memory for sound data.\n");
+        CloseHandle(hFile);
+        return;
+    }
+
+    DWORD bytes_read;
+    if (!ReadFile(hFile, sound_data, sound_size, &bytes_read, NULL)) {
+        printf("Failed to read file: %s\n", filename);
+        free(sound_data);
+        CloseHandle(hFile);
+        return;
+    }
+
+    CloseHandle(hFile);
+
+    play_sound_from_memory(sound_data, sound_size, SND_ASYNC);
+
+    free(sound_data);
 }
 
 
-void play_background_music(const char* filename) {
-    PlaySound(filename, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
-}
+void play_background_music(void* arg) {
+    const char* filename = "background_music.wav";
+    HANDLE hFile = CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf("Failed to open file: %s\n", filename);
+        return;
+    }
 
+    DWORD sound_size = GetFileSize(hFile, NULL);
+    if (sound_size == INVALID_FILE_SIZE) {
+        printf("Failed to get file size: %s\n", filename);
+        CloseHandle(hFile);
+        return;
+    }
+
+    void* sound_data = malloc(sound_size);
+    if (sound_data == NULL) {
+        printf("Failed to allocate memory for sound data.\n");
+        CloseHandle(hFile);
+        return;
+    }
+
+    DWORD bytes_read;
+    if (!ReadFile(hFile, sound_data, sound_size, &bytes_read, NULL)) {
+        printf("Failed to read file: %s\n", filename);
+        free(sound_data);
+        CloseHandle(hFile);
+        return;
+    }
+
+    CloseHandle(hFile);
+
+    PlaySound(filename, NULL, SND_FILENAME | SND_LOOP | SND_ASYNC);
+
+    free(sound_data);
+}
 int main() {
-    // Start playing the background music in a separate thread
-    const char* bg_music_filename = "bg_music.wav";
-    PlaySound(bg_music_filename, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+HANDLE hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)play_background_music, NULL, 0, NULL);
+    if (hThread == NULL) {
+        printf("Error creating thread.\n");
+        return 1;
+    }
+while (1) {
+    // Get user input and update the game state
 
-// Below is hypothetical code so the sounds can be implemented it will most likely come into play when george codes the correct
-// and wrong guesses
-
-/*
-    // Game loop
-    while (1) {
-        // Get user input and update the game state
-
-        // If the user makes a correct guess, play the correct sound
-        if (user_makes_correct_guess()) {
+    // Check for user input
+    if (_kbhit()) {
+        int input = getchar();
+        switch (input) {
+        case 'c':
             play_correct_sound();
-        }
-        // If the user makes a wrong guess, play the wrong sound
-        else if (user_makes_wrong_guess()) {
+            break;
+        case 'w':
             play_wrong_sound();
-        }
-
-        // Check if the game is over
-        if (game_is_over()) {
             break;
         }
+
+        // Clear input buffer
+        while (getchar() != '\n') {
+            // do nothing
+        }
     }
-*/
-    return 0;
 }
 
+    // Wait for the background music thread to exit
+    WaitForSingleObject(hThread, INFINITE);
+    CloseHandle(hThread);
+
+    return 0;
+}
