b9c34438b544803c21dd806a81962f802f4a2120
Gunkel <gunkeec@dunwoody.edu>
2023-04-28 14:05:20 -0500

    Switched libraries and able to stack sounds finally. Just need to changed the if statements to make more sense for the implementation of the game

--- sounds.c
+++ sounds.c
@@ -1,114 +1,61 @@
+#include <SDL2/SDL.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <Windows.h>
-#include <conio.h>
-#include <process.h>
-#pragma comment(lib, "winmm.lib")
 
 
-void play_sound(const char* filename) {
-    PlaySound(filename, NULL, SND_FILENAME);
-}
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_AUDIO);
 
-void play_sound_from_memory(void* sound_data, DWORD sound_size, DWORD flags) {
-    PlaySound(sound_data, NULL, SND_MEMORY | SND_ASYNC | flags);
-}
+    SDL_AudioSpec wavSpec;
+    Uint8* bgmBuffer;
+    Uint32 bgmLength;
 
-void play_correct_sound() {
-    const char* filename = "correct_sound.wav";
-    HANDLE hFile = CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE) {
-        printf("Failed to open file: %s\n", filename);
-        return;
-    }
+    // Load background music into memory
+    SDL_LoadWAV("background_music.wav", &wavSpec, &bgmBuffer, &bgmLength);
+    SDL_AudioDeviceID bgmDeviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
 
-    DWORD sound_size = GetFileSize(hFile, NULL);
-    if (sound_size == INVALID_FILE_SIZE) {
-        printf("Failed to get file size: %s\n", filename);
-        CloseHandle(hFile);
-        return;
-    }
+    // Queue and unpause the background music
+    SDL_QueueAudio(bgmDeviceId, bgmBuffer, bgmLength);
+    SDL_PauseAudioDevice(bgmDeviceId, 0);
 
-    void* sound_data = malloc(sound_size);
-    if (sound_data == NULL) {
-        printf("Failed to allocate memory for sound data.\n");
-        CloseHandle(hFile);
-        return;
-    }
+    // Load sound effects for guessing right and wrong into memory
+    Uint8* rightBuffer;
+    Uint32 rightLength;
+    SDL_LoadWAV("correct_sound.wav", &wavSpec, &rightBuffer, &rightLength);
+    SDL_AudioDeviceID rightDeviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
 
-    DWORD bytes_read;
-    if (!ReadFile(hFile, sound_data, sound_size, &bytes_read, NULL)) {
-        printf("Failed to read file: %s\n", filename);
-        free(sound_data);
-        CloseHandle(hFile);
-        return;
-    }
+    Uint8* wrongBuffer;
+    Uint32 wrongLength;
+    SDL_LoadWAV("wrong_sound.wav", &wavSpec, &wrongBuffer, &wrongLength);
+    SDL_AudioDeviceID wrongDeviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
 
-    CloseHandle(hFile);
+    // Play guessing game with sound effects
+    int guess = 0;
+    while (1) {
+        printf("Guess a number: ");
+        scanf("%d", &guess);
 
-    play_sound_from_memory(sound_data, sound_size, SND_ASYNC);
-
-    free(sound_data);
-}
-
-void play_wrong_sound() {
-    const char* filename = "wrong_sound.wav";
-    HANDLE hFile = CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE) {
-        printf("Failed to open file: %s\n", filename);
-        return;
-    }
-
-    DWORD sound_size = GetFileSize(hFile, NULL);
-    if (sound_size == INVALID_FILE_SIZE) {
-        printf("Failed to get file size: %s\n", filename);
-        CloseHandle(hFile);
-        return;
-    }
-
-    void* sound_data = malloc(sound_size);
-    if (sound_data == NULL) {
-        printf("Failed to allocate memory for sound data.\n");
-        CloseHandle(hFile);
-        return;
-    }
-
-    DWORD bytes_read;
-    if (!ReadFile(hFile, sound_data, sound_size, &bytes_read, NULL)) {
-        printf("Failed to read file: %s\n", filename);
-        free(sound_data);
-        CloseHandle(hFile);
-        return;
-    }
-
-    CloseHandle(hFile);
-
-    play_sound_from_memory(sound_data, sound_size, SND_ASYNC);
-
-    free(sound_data);
-}
-
-int main() {
-while (1) {
-    // Get user input and update the game state
-
-    // Check for user input
-    if (_kbhit()) {
-        int input = getchar();
-        switch (input) {
-        case 'c':
-            play_correct_sound();
-            break;
-        case 'w':
-            play_wrong_sound();
-            break;
-        }
-
-        // Clear input buffer
-        while (getchar() != '\n') {
-            // do nothing
+        if (guess == 42) {
+            // Play sound effect for guessing right
+            SDL_QueueAudio(rightDeviceId, rightBuffer, rightLength);
+            SDL_PauseAudioDevice(rightDeviceId, 0);
+        } else {
+            // Play sound effect for guessing wrong
+            SDL_QueueAudio(wrongDeviceId, wrongBuffer, wrongLength);
+            SDL_PauseAudioDevice(wrongDeviceId, 0);
         }
     }
-}
+
+    // Clean up audio devices and free sound data
+    SDL_CloseAudioDevice(bgmDeviceId);
+    SDL_FreeWAV(bgmBuffer);
+
+    SDL_CloseAudioDevice(rightDeviceId);
+    SDL_FreeWAV(rightBuffer);
+
+    SDL_CloseAudioDevice(wrongDeviceId);
+    SDL_FreeWAV(wrongBuffer);
+
+    SDL_Quit();
+
     return 0;
 }
