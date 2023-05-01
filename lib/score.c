6e315ba3b1d74ec583f4c88fe8dc9a174a646eb5
GeorgeJShea <shea.george2@gmail.com>
2023-05-01 15:09:59 -0500

    basic implmentation of score function

--- lib/score.c
+++ lib/score.c
@@ -1,10 +1,58 @@
 #include <stdio.h>
 #include "words.h"
+#include <string.h>
 
-int check() 
+
+int check()
 {
     Word word = getHangmanWord();
-    printf("raaaaaaaaaaaaaaaaa   %s", word.letters);
+    printf("raaaaaaabbbbaaaaaa   %s", word.letters);
+    printf("\n");
+
+    char guess[26];
+
+    printf("Enter word: ");
+    scanf("%26s", guess);
+
+    printf("you pailed: %s\n", guess);
+    int guessLen = strlen(guess);
+
+    char wrongGuesses[26];
+    char correctGuesses[26];
+
+    int correct = 0;
+    // change this later
+    int lives = 3;
+    for (int guessinx = 0; guessinx < guessLen; guessinx++)
+    {
+        for (int wordinx = 0; wordinx < 32; wordinx++)
+        {
+            if (guess[guessinx] == word.letters[wordinx])
+            {
+                correct = 1;
+            }
+        }
+        if (correct == 1)
+        {
+            printf("Your smart");
+            correctGuesses[guessinx] = guess[guessinx];
+            printf("\n");
+        }
+        else
+        {   
+            
+            
+            printf("You guessed wrong ");
+            wrongGuesses[guessinx] = guess[guessinx];
+            printf("\n");
+        }
+        correct = 0;
+        
+
+    }
+
+    printf("\n");
+
     freeWord(word);
 
     return 0;
