f9b50694f028e746fcef383d6e1fe6d87312a30c
Cooper Hanson <hancooj@dunwoody.edu>
2023-05-11 20:28:05 -0500

    Forgot to include the preprocessor stuff

--- lib/util.h
+++ lib/util.h
@@ -1,3 +1,5 @@
+#ifndef UTIL_H
+#define UTIL_H
 #include <stdbool.h>
 char upper(char c);
 bool is_alphabetic(char c);
@@ -7,3 +9,4 @@ bool bit_set(unsigned bm, int pos);
 unsigned set_bit(unsigned bm, int pos);
 
 unsigned letter_positions(char* str, char c);
+#endif
