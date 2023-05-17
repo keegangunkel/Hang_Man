0fc0b910463844147e5e357542a8478d9e3da6e7
Cooper Hanson <hancooj@dunwoody.edu>
2023-05-17 18:53:18 -0500

    Enumerated the audios

--- lib/sound.c
+++ lib/sound.c
@@ -4,23 +4,34 @@
 
 /* Library functions found here https://wiki.libsdl.org/SDL2/CategoryAPI */
 
-AudioData loadAudioData(const char* path) {
+AudioData loadAudio(AudioEffect sound) {
   AudioData audio = { 0 };
+  const char* path;
+  switch (sound) {
+    case BACKGROUND_MUSIC:
+      path = "sounds/background_music.wav";
+      break;
+    case CORRECT_SOUND:
+      path = "sounds/correct_sound.wav";
+      break;
+    case WRONG_SOUND:
+      path = "sounds/wrong_sound.wav";
+      break;
+  }
+
   SDL_AudioSpec* I_dont_know_what_this_returns = SDL_LoadWAV(path, &audio.wavSpec, &audio.buffer, &audio.length);
   if (!I_dont_know_what_this_returns) {
     fprintf(stderr, "Failed to load the audio file: %s\n", path);
     exit(1);
   }
-  audio.id = SDL_OpenAudioDevice(NULL, 0, &audio.wavSpec, NULL, 0);
+  audio.id = SDL_OpenAudioDevice(NULL, 0, &audio.wavSpec, NULL, 0); // apparently the last 0 is for latency?
   return audio;
 }
 
-/*
-void playBackgroundMusic(AudioData audio) {
-    // Queue and unpause the background music
-    SDL_QueueAudio(audio.bgmDeviceId, audio.bgmBuffer, audio.bgmLength);
-    SDL_PauseAudioDevice(audio.bgmDeviceId, 0);
-} */
+void playAudio(AudioData audio) {
+  SDL_QueueAudio(audio.id, audio.buffer, audio.length);
+  SDL_PauseAudioDevice(audio.id, 0);
+}
 
 void cleanupAudio(AudioData audio) {
   SDL_CloseAudioDevice(audio.id);
--- lib/sound.h
+++ lib/sound.h
@@ -1,6 +1,12 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h> // Something to look into? https://github.com/libsdl-org/SDL_mixer -- playwave.c#Mix_PlayChannel
 
+typedef enum {
+  BACKGROUND_MUSIC,
+  CORRECT_SOUND,
+  WRONG_SOUND
+} AudioEffect;
+
 typedef struct {
   SDL_AudioDeviceID id;
   Uint8* buffer;
